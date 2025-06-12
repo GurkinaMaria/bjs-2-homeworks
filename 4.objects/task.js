de924883a39bbfce4f3bcdd0239a4c082d7de489
Maria <erita93@mail.ru>
2025-06-12 21:07:17 +0300

    Четвертое задание полностью готово, дубль 2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,36 +1,32 @@
 function Student(name, gender, age) {
-  this.name = "Василиса";
-  this.gender = "женский";
-  this.age = 19;
-  this.marks = [];
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
-let student = new Student;
-console.log(student.name);
+let student = new Student("Василиса", "женский", 19);
 
 Student.prototype.setSubject = function (subjectName) {
- let setSubject = "Algebra";
- this.subject = subjectName;
+    this.subject = subjectName;
 }
 
-console.log(student.setSubject);
-
 Student.prototype.addMarks = function (...marks) {
-    if (this.marks) { 
-    this.marks.push(...marks); 
-  }
+    if (this.marks) {
+        this.marks.push(...marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-   if (!this.marks || this.marks.length === 0) {
-    return 0;
-  }
-  let sum = this.marks.reduce((acc, mark) => acc + mark, 0); 
-  return sum / this.marks.length; 
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    }
+    let sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+    return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  delete this.subject;
-  delete this.marks;
-  this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
