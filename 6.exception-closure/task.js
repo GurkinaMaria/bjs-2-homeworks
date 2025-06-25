686a7ae32ed363df2cd563c9563135536775b315
Maria <erita93@mail.ru>
2025-06-25 23:45:26 +0300

    задание полностью готово, дубль2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,15 +1,15 @@
 ﻿function parseCount(value) {
-  const parsedValue = Number.parseFloat(value); // Преобразуем строку в число
-  if (isNaN(parsedValue)) { // Проверяем, является ли результат NaN
-    throw new Error("Невалидное значение"); // Генерируем ошибку
+  const parsedValue = Number.parseFloat(value);
+  if (isNaN(parsedValue)) {
+    throw new Error("Невалидное значение");
   }
-  return parsedValue; // Возвращаем распарсенное значение
+  return parsedValue;
 }
 function validateCount(value) {
   try {
-    return parseCount(value); // Пытаемся распарсить значение
+    return parseCount(value);
   } catch (error) {
-    return error; // Возвращаем ошибку в случае перехвата исключения
+    return error;
   }
 }
 
@@ -17,30 +17,32 @@ function validateCount(value) {
 
 class Triangle {
   constructor(a, b, c) {
-    // Проверка существования треугольника
     if (a + b <= c || a + c <= b || b + c <= a) {
       throw new Error("Треугольник с такими сторонами не существует");
     }
-    this.a = a; // Сторона a
-    this.b = b; // Сторона b
-    this.c = c; // Сторона c
+    this.a = a;
+    this.b = b;
+    this.c = c;
   }
   get perimeter() {
-    return this.a + this.b + this.c; // Вычисление периметра
+    return this.a + this.b + this.c;
   }
   get area() {
-    const s = this.perimeter / 2; // Полупериметр
-    // Вычисление площади по формуле Герона
+    const s = this.perimeter / 2;
     return parseFloat(Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c)).toFixed(3));
   }
 }
 function getTriangle(a, b, c) {
   try {
-    return new Triangle(a, b, c); // Пытаемся создать новый объект треугольника
+    return new Triangle(a, b, c);
   } catch (error) {
     return {
-      area: () => "Ошибка! Треугольник не существует", // Возвращаем объект с сообщением об ошибке
-      perimeter: () => "Ошибка! Треугольник не существует"
-    }
-}
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";а
+      }
+    };
+  }
 }
